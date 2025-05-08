4c8c27e2be32c4893aabc1a416d49c7e120aca23
장윤정 <wkddbs3935@naver.com>
2025-05-08 20:43:34 +0900

    INITIAL COMMIT

--- src/main/java/com/example/lifedebug/global/config/SwaggerConfig.java
+++ src/main/java/com/example/lifedebug/global/config/SwaggerConfig.java
@@ -1,4 +1,31 @@
 package com.example.lifedebug.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components().addSecuritySchemes("Authorization",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("해커톤 주제~~ API")
+                .description("25-1 해커톤 인생디버깅팀")
+                .version("1.0.0");
+    }
 }
